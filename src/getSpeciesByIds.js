376f08ad2d1f81283b2e63bf7ea2efe7561c3c0e
UrielSilvaDeMoura <urielsilvademoura@outlook.com>
2022-07-28 16:24:52 -0300

    resolução requisito 1 com lint ajustado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,6 +3,5 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   return ids.map((id) => data.species.find((especie) => especie.id === id));
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 
 module.exports = getSpeciesByIds;
