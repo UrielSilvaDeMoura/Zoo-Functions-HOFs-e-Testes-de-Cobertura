574092fe2dbd90332b0b8d3463015e23aff5862a
UrielSilvaDeMoura <urielsilvademoura@outlook.com>
2022-07-28 18:21:36 -0300

    resolução requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  if (data.employees.find((gerente) => gerente.managers.find((cargo) => cargo === id))) {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const colaboradores = data.employees.filter((colaborador) =>
+    colaborador.managers.includes(managerId));
+  const nomeColaboradores = colaboradores.map((colaborador) =>
+    `${colaborador.firstName} ${colaborador.lastName}`);
+  return nomeColaboradores;
 }
 
 module.exports = { isManager, getRelatedEmployees };
