561260d43fcbf92030537818ac029b07328506df
UrielSilvaDeMoura <urielsilvademoura@outlook.com>
2022-07-29 19:42:46 -0300

    resolução requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const responsavel = data.employees.find((colaborador) => colaborador.id === id).responsibleFor[0];
+  const animalColaborador = data.species.filter((animal) => animal.id === responsavel);
+  const animalMaisVelho = animalColaborador[0].residents.reduce((acc, curr) => {
+    if (acc.age > curr.age) {
+      return acc;
+    }
+    return curr;
+  });
+
+  const { name, sex, age } = animalMaisVelho;
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
