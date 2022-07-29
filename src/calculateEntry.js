1f3b690cd3b2153e148ae3153f8cd3f98c4992b0
UrielSilvaDeMoura <urielsilvademoura@outlook.com>
2022-07-29 18:44:26 -0300

    resolução requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((pessoa) => pessoa.age < 18).length;
+  const adult = entrants.filter((pessoa) => pessoa.age >= 18 && pessoa.age < 50).length;
+  const senior = entrants.filter((pessoa) => pessoa.age >= 50).length;
+  return {
+    child,
+    adult,
+    senior,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Array.isArray(entrants) !== true) return 0;
+  const pessoas = countEntrants(entrants);
+  const preço = data.prices;
+  const ingressoCriança = pessoas.child * preço.child;
+  const ingressoAdulto = pessoas.adult * preço.adult;
+  const ingressoIdoso = pessoas.senior * preço.senior;
+  const valorTotal = ingressoCriança + ingressoAdulto + ingressoIdoso;
+  return valorTotal;
 }
 
 module.exports = { calculateEntry, countEntrants };
