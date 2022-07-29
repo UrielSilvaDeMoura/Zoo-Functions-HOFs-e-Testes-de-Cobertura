2f4630dc527248850faea8fd79e28a518205c6be
UrielSilvaDeMoura <urielsilvademoura@outlook.com>
2022-07-29 16:16:14 -0300

    resolução requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const quant = {};
+    data.species.forEach((specie) => {
+      quant[specie.name] = specie.residents.length;
+    });
+    return quant;
+  }
+  const nomeAnimal = data.species.find((especieDoAnimal) => especieDoAnimal.name === animal.specie);
+
+  if (animal.sex) {
+    const sexoAnimal = nomeAnimal.residents.filter((sexoDoAnimal) =>
+      sexoDoAnimal.sex === animal.sex);
+    return sexoAnimal.length;
+  }
+  return nomeAnimal.residents.length;
 }
 
 module.exports = countAnimals;
